187aea8c4dd614406aecbc98f74f9e341b751ce4
Jayme Laso-Barros <jayme@zenit.senecac.on.ca>
2016-01-21 16:41:01 -0500

    Added basic frontend layout for Sign In page.

---
+++ signin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Account Creation - Open Data Visualizer</title>
+  </head>
+  <body>
+    <span>Account Creation</span><br>
+    <form>
+      <div>
+        Username:<br>
+        <input type="text" name="username"><br>
+      </div>
+      <div>
+        Password:<br>
+        <input type="password" name="password"><br>
+      </div>
+    </form>
+  </body>
+</html>
